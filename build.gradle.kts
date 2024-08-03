adb1c77dd90d593917f7fca43ee39cec46b70888
Chulkov Alexey <chulkovalexey18@gmail.com>
2024-08-03 18:31:37 +0300

    migrate to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    id("java")
+    id("antlr")
+}
+
+group = "com.github"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+val junitJupiterVersion = "5.9.2"
+val junitPlatformVersion = "1.9.2"
+val antlrPluginVersion = "4.13.1"
+val sfl4jVersion = "2.0.6"
+
+dependencies {
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.7.0.202309050840-r")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
+    testImplementation("org.junit.platform:junit-platform-suite:$junitPlatformVersion")
+
+    testImplementation("org.mockito:mockito-core:5.2.0")
+
+    implementation("org.lmdbjava:lmdbjava:0.8.3")
+
+    implementation("org.slf4j:slf4j-api:$sfl4jVersion")
+    implementation("org.slf4j:slf4j-reload4j:$sfl4jVersion")
+
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+
+    // https://mvnrepository.com/artifact/org.antlr/antlr4-runtime
+    implementation("org.antlr:antlr4-runtime:$antlrPluginVersion")
+    antlr("org.antlr:antlr4:$antlrPluginVersion")
+}
+val generatedModule = "${projectDir}/src/main/java"
+val generationOutput = "$generatedModule/com/github/SuduIDE/persistentidecaches/javaparaser"
+
+sourceSets {
+    main {
+        java {
+            srcDir(generatedModule)
+        }
+    }
+}
+
+tasks.named<AntlrTask>("generateGrammarSource") {
+    maxHeapSize = "64m"
+    arguments.addAll(listOf("-visitor", "-long-messages", "-package", "com.github.SuduIDE.persistentidecaches.javaparaser"))
+    outputDirectory = file(generationOutput)
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        attributes["Main-Class"] = "com.github.SuduIDE.persistentidecaches.VsCodeClient"
+    }
+
+    // Ensure the runtime classpath also includes ANTLR generated sources
+//    from(configurations.runtimeClasspath.map { if (it.isDirectory) it else zipTree(it) })
+
+    archiveFileName.set("${project.name}-${project.version}-all.jar")
+}
+
+tasks.withType<JavaCompile> {
+    dependsOn("generateGrammarSource")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.clean {
+    doLast {
+        fileTree(generationOutput).dir.deleteRecursively()
+    }
+
+}
--- src/main/java/com/github/SuduIDE/persistentidecaches/ccsearch/JavaSymbolListener.java
+++ src/main/java/com/github/SuduIDE/persistentidecaches/ccsearch/JavaSymbolListener.java
@@ -1,28 +1,16 @@
 package com.github.SuduIDE.persistentidecaches.ccsearch;
 
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaLexer;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.ClassDeclarationContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.ConstDeclarationContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.ConstantDeclaratorContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.EnumDeclarationContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.FieldDeclarationContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.InterfaceDeclarationContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.InterfaceMethodDeclarationContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.LocalTypeDeclarationContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.LocalVariableDeclarationContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.MethodDeclarationContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.RecordDeclarationContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.VariableDeclaratorContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParser.VariableDeclaratorIdContext;
-import com.github.SuduIDE.persistentidecaches.grammars.java.JavaParserBaseListener;
+import com.github.SuduIDE.persistentidecaches.javaparaser.JavaLexer;
+import com.github.SuduIDE.persistentidecaches.javaparaser.JavaParser;
+import com.github.SuduIDE.persistentidecaches.javaparaser.JavaParserBaseListener;
 import com.github.SuduIDE.persistentidecaches.symbols.Symbols;
-import java.util.ArrayList;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.util.ArrayList;
 
 public class JavaSymbolListener extends JavaParserBaseListener {
 
@@ -40,62 +28,62 @@ public class JavaSymbolListener extends JavaParserBaseListener {
     }
 
     @Override
-    public void enterClassDeclaration(final ClassDeclarationContext ctx) {
+    public void enterClassDeclaration(final JavaParser.ClassDeclarationContext ctx) {
         symbols.classOrInterfaceSymbols().add(ctx.identifier().getText());
     }
 
     @Override
-    public void enterEnumDeclaration(final EnumDeclarationContext ctx) {
+    public void enterEnumDeclaration(final JavaParser.EnumDeclarationContext ctx) {
         symbols.classOrInterfaceSymbols().add(ctx.identifier().getText());
     }
 
     @Override
-    public void enterInterfaceDeclaration(final InterfaceDeclarationContext ctx) {
+    public void enterInterfaceDeclaration(final JavaParser.InterfaceDeclarationContext ctx) {
         symbols.classOrInterfaceSymbols().add(ctx.identifier().getText());
     }
 
     @Override
-    public void enterFieldDeclaration(final FieldDeclarationContext ctx) {
+    public void enterFieldDeclaration(final JavaParser.FieldDeclarationContext ctx) {
         ctx.variableDeclarators().variableDeclarator().stream()
-                .map(VariableDeclaratorContext::variableDeclaratorId)
-                .map(VariableDeclaratorIdContext::identifier)
+                .map(JavaParser.VariableDeclaratorContext::variableDeclaratorId)
+                .map(JavaParser.VariableDeclaratorIdContext::identifier)
                 .map(RuleContext::getText)
                 .forEach(symbols.fieldSymbols()::add);
     }
 
     @Override
-    public void enterConstDeclaration(final ConstDeclarationContext ctx) {
+    public void enterConstDeclaration(final JavaParser.ConstDeclarationContext ctx) {
         symbols.fieldSymbols().addAll(
                 ctx.constantDeclarator().stream()
-                        .map(ConstantDeclaratorContext::identifier)
+                        .map(JavaParser.ConstantDeclaratorContext::identifier)
                         .map(RuleContext::getText)
                         .toList()
         );
     }
 
     @Override
-    public void enterInterfaceMethodDeclaration(final InterfaceMethodDeclarationContext ctx) {
+    public void enterInterfaceMethodDeclaration(final JavaParser.InterfaceMethodDeclarationContext ctx) {
         symbols.methodSymbols().add(ctx.interfaceCommonBodyDeclaration().identifier().getText());
     }
 
     @Override
-    public void enterRecordDeclaration(final RecordDeclarationContext ctx) {
+    public void enterRecordDeclaration(final JavaParser.RecordDeclarationContext ctx) {
         symbols.classOrInterfaceSymbols().add(ctx.identifier().getText());
     }
 
     @Override
-    public void enterMethodDeclaration(final MethodDeclarationContext ctx) {
+    public void enterMethodDeclaration(final JavaParser.MethodDeclarationContext ctx) {
         symbols.methodSymbols().add(ctx.identifier().getText());
     }
 
     // we need it?
     @Override
-    public void enterLocalVariableDeclaration(final LocalVariableDeclarationContext ctx) {
+    public void enterLocalVariableDeclaration(final JavaParser.LocalVariableDeclarationContext ctx) {
         super.enterLocalVariableDeclaration(ctx);
     }
 
     @Override
-    public void enterLocalTypeDeclaration(final LocalTypeDeclarationContext ctx) {
+    public void enterLocalTypeDeclaration(final JavaParser.LocalTypeDeclarationContext ctx) {
         super.enterLocalTypeDeclaration(ctx);
     }
 }
